6fb4a1e4348949c076b1015e6f73cf79704f249f
J R Douceur <115005855+qudn@users.noreply.github.com>
2023-06-28 22:40:11 +0800

    Update sdk.h

--- sdk.h
+++ sdk.h
@@ -1,43 +1,54 @@
+/*
+ * Copyright (c) 2023 LYC&MXY
+ */
 #pragma once
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <string>
 
-using namespace std;
 
-typedef vector<vector<char> > Board; //存储数独数据
+using std::ifstream;
+using std::ofstream;
+using std::vector;
+using std::string;
+
+
+typedef vector<vector<char> > Board;  // 存储数独数据
 
 static const int N = 9;
-class sdk
-{
-private:
+class sdk {
+ private:
     // 使用位运算来表示在某行、列、块中某个数是否出现过
     int rowUsed[N];
     int columnUsed[N];
     int blockUsed[N];
 
-public:
-    vector<Board> result; // 存储数独结果
-    vector<pair<int, int> > spaces; //存储尚未填充的数独坐标
-    
-public:
-    sdk()
-    {
+ public:
+    vector<Board> result;  // 存储数独结果
+    vector<pair<int, int> > spaces;  // 存储尚未填充的数独坐标
+
+    sdk() {
         initState();
     }
     void initState();
 
     Board generateBoard(int digNum);
-    bool checkBoard(Board& board);
-    void copyNineSquareGrid(Board& board, int src_x, int src_y, bool isRow);
+    bool checkBoard(Board* board);
+    void copyNineSquareGrid(Board* board, int src_x, int src_y, bool isRow);
     vector<int> getRand9();
     vector<Board> solveSudoku(Board board);
     void UpdateCoordinate(int i, int j, int digit);
-    void DFS(Board& board, int pos);
+    void DFS(Board* board, int pos);
 };
 
 vector<Board> readFile(string filePath);
 void writeFile(vector<Board> board, ofstream& f);
 
-void generateGame(int gameNumber, int gameLevel, vector<int> digNum, ofstream& outfile, sdk& player);
+void generateGame(int gameNumber,
+                  int gameLevel,
+                  vector<int> digNum,
+                  ofstream& outfile,
+                  sdk* player);
 
 
